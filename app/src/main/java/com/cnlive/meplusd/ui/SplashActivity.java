036855bf93d268e881761f9aac41375379a4f150
zhangshuai@cnlive.com <zhangshuai@cnlive.com>
2017-02-09 10:30:38 +0800

    ..

--- app/src/main/java/com/cnlive/meplusd/ui/SplashActivity.java
+++ app/src/main/java/com/cnlive/meplusd/ui/SplashActivity.java
@@ -2,8 +2,8 @@ package com.cnlive.meplusd.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.os.Handler;
+import android.view.KeyEvent;
 import android.view.View;
 
 import com.cnlive.meplusd.R;
@@ -12,11 +12,9 @@ import com.facebook.drawee.view.SimpleDraweeView;
 
 import butterknife.Bind;
 
-import static com.cnlive.meplusd.R.id.simpleImage1;
-
 public class SplashActivity extends BaseActivity implements View.OnClickListener {
 
-    @Bind(simpleImage1)
+    @Bind(R.id.simpleImage1)
     SimpleDraweeView mSimpleImage1;
 
     @Override
@@ -24,7 +22,7 @@ public class SplashActivity extends BaseActivity implements View.OnClickListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        mSimpleImage1.setImageResource(R.drawable.splash);
+        //mSimpleImage1.setImageResource(R.drawable.splash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
