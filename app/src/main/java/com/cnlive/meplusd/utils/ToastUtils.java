ba2225f4e75499ecd854dc30f06109ce023e6636
zhangshuai@cnlive.com <zhangshuai@cnlive.com>
2017-02-10 10:42:41 +0800

    适配弹出顶部提示框

--- app/src/main/java/com/cnlive/meplusd/utils/ToastUtils.java
+++ app/src/main/java/com/cnlive/meplusd/utils/ToastUtils.java
@@ -1,6 +1,7 @@
 package com.cnlive.meplusd.utils;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -98,14 +99,45 @@ public class ToastUtils {
     }
 
 
+    /*
+    * 底部提示
+    * @param context
+    * @param msg
+    * */
     public static void CustomToast(Context context,String msg){
         Toast toast=null;
         if(toast == null){ toast = new Toast(context); }
         toast.setDuration(Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.BOTTOM, 0, 100);
-        View view=LayoutInflater.from(context).inflate(R.layout.util_toast,null);
+        View view=LayoutInflater.from(context).inflate(R.layout.toast_down,null);
         TextView txtToast = (TextView)view.findViewById(R.id.tv_toast);
         txtToast.setText("\t"+msg+" \t");
+        toast.setView(view);
+        toast.show();
+    }
+
+
+    /*
+    * 顶部提示
+    * @param context
+    * @param msg
+    * */
+    public static void CustomTopToast(Context context,String msg){
+        Toast toast=null;
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view=inflater.inflate(R.layout.toast_top,null);
+        TextView txtToast = (TextView)view.findViewById(R.id.tv_toast);
+
+        if (!TextUtils.isEmpty(msg)){ txtToast.setText("\t"+msg+" \t"); }
+
+        if(toast == null){
+            toast = new Toast(context);
+            toast.setDuration(Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.FILL_HORIZONTAL|Gravity.TOP, 0, 0);
+        }
+
         toast.setView(view);
         toast.show();
     }
